8f443bfd1c467e07bf3408b20ba387edcb332e0c
senzyuu <arozs08070@yahoo.co.jp>
2019-05-03 17:38:00 +0900

    new,create完成!

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,13 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def  new
+    @book = Book.new
+  end
+  def  create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to @book, notice: "書籍を登録しました｡"
+  end
+
 end
