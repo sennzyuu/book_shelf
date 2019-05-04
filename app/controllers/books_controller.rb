6e6eea9e0fee4417df851b7a2678979304077d8b
senzyuu <arozs08070@yahoo.co.jp>
2019-05-04 11:55:12 +0900

    リファクタリング機能追加｡

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
   def index
     @books = Book.all
   end
@@ -16,15 +17,13 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[:id])
+   
   end
 
   def edit
-    @book = Book.find(params[:id])
   end
   
   def update
-   @book = Book.find(params[:id])
    if @book.update(book_params)
      redirect_to @book, notice: "書籍を更新しました。"
    else
@@ -33,7 +32,6 @@ class BooksController < ApplicationController
   end
 
   def destroy
-     @book = Book.find(params[:id])
      @book.destroy
      redirect_to books_path, notice: "書籍を削除しました。"
   end
@@ -45,5 +43,8 @@ class BooksController < ApplicationController
     params.require(:book).permit(:title, :price, :publish_date, :description, :new_image)
   end
    
+  def set_up
+    @book = Book.find(params[:id])
+  end
 
 end
