d5d71b2e6744d535a404d6af11bded5afff43009
senzyuu <arozs08070@yahoo.co.jp>
2019-05-04 17:24:29 +0900

    レビュー投稿登録機能作成

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :category
+  has_many :reviews, dependent: :destroy
   has_one_attached :image
-  
   attribute :new_image
   validates :title, presence: true,
   length: { maximum: 50 }
